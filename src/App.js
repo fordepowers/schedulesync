ea708974ee8f192919ddb26cc58c08c6bc654082
Ford Powers <fordepowers@gmail.com>
2019-10-12 14:11:56 -0700

    Add NavBarCustom, About, CreateNewForm

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import About from './About/About';
+import CreateNewForm from './CreateNewForm/CreateNewForm'
+import NavbarCustom from './NavbarCustom/NavbarCustom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <NavbarCustom Text="Create New" Route="/create-new" />
+            <About />
+          </Route>
+          <Route path="/create-new">
+            <NavbarCustom Text="Home" Route="/" />
+            <CreateNewForm />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
