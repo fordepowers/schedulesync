ddf3fffad59ee0d86471f347870c91827bf507f0
Ford Powers <fordepowers@gmail.com>
2019-10-12 20:46:17 -0700

    rework CreateNewForm.js

--- src/CreateNewForm/CreateNewForm.js
+++ src/CreateNewForm/CreateNewForm.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './CreateNewForm.css';
-import { Button, Form, Col, Row, ButtonToolbar, InputGroup, DropdownButton } from 'react-bootstrap';
+import { Button, Form, Col, Row, InputGroup, Alert } from 'react-bootstrap';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Link } from 'react-router-dom';
 import firebase from '../firebase/firebase';
 
 const INITIAL_FORM_STATE = {
-  title: 'Bobbys Birthday',
-  description: 'His 27th',
   dayOfWeek: 'Monday',
-  isAllDay: true,
   from: 60,
   fromAMPM: 'AM',
   to: 60,
@@ -44,6 +44,13 @@ class CreateNewForm extends React.Component {
     });
   }
 
+  handleChange = date => {
+    this.setState({
+      ...this.state,
+      selectedDate: date
+    });
+  };
+
   handleToggle = event => {
 
     console.log(event.target.checked);
@@ -58,7 +65,7 @@ class CreateNewForm extends React.Component {
     minutes = Number(minutes);
 
     if (minutes === 0 || minutes === 720) {
-      if (qualifier == 'AM') {
+      if (qualifier === 'AM') {
         minutes = 0;
       }
       else {
@@ -66,7 +73,7 @@ class CreateNewForm extends React.Component {
       }
     }
     else {
-      if (qualifier == 'AM') {
+      if (qualifier === 'AM') {
         // return minutes;
       }
       else {
@@ -80,19 +87,20 @@ class CreateNewForm extends React.Component {
   submitForm = () => {
     let from = this.convertTo24Hour(this.state.from, this.state.fromAMPM);
     let to = this.convertTo24Hour(this.state.to, this.state.toAMPM);
+    let selectedDate = this.state.selectedDate.toString();
 
     if (this.state.isAllDay) {
       from = 0;
       to = 1440;
     }
-    
+
     let form = {
       title: this.state.title,
       description: this.state.description,
       isAllDay: true,
       from: from,
       to: to,
-      dayOfWeek: this.state.dayOfWeek
+      selectedDate: selectedDate
     };
     console.dir(form);
 
@@ -103,96 +111,94 @@ class CreateNewForm extends React.Component {
   }
 
   render() {
-    const { title, description, from, isAllDay } = this.state;
-    // const { from } = this.state.options;
+    const { title, description, isAllDay } = this.state;
 
     return (
       <div className='CreateNewForm'>
-        <Form>
-          <Form.Group as={Row}>
-            <Form.Label column sm="2">
-              Title
+        <Alert variant='light'>
+          <Form>
+            <Form.Group as={Row}>
+              <Form.Label column sm="2">
+                Event Title
             </Form.Label>
-            <Col sm="10">
-              <Form.Control type="plaintext" onChange={this.onChange} name="title" placeholder="Title" value={title} />
-            </Col>
-          </Form.Group>
-        </Form>
+              <Col sm="10">
+                <Form.Control type="plaintext" onChange={this.onChange} name="title" placeholder="'Club Meeting'" value={title} />
+              </Col>
+            </Form.Group>
+          </Form>
 
-        <Form>
-          <Form.Group as={Row}>
-            <Form.Label column sm={2}>
-              Description
-          </Form.Label>
-            <Col sm={10}>
-              <Form.Control type="plaintext" onChange={this.onChange} value={description} name="description" placeholder="Enter description of event:" />
-            </Col>
+          <Form>
+            <Form.Group as={Row}>
+              <Form.Label column sm={2}>
+                Event Description
+              </Form.Label>
+              <Col sm={10}>
+                <Form.Control type="plaintext" onChange={this.onChange} value={description} name="description" placeholder="'What time can you meet?'" />
+              </Col>
+            </Form.Group>
 
-          </Form.Group>
+            <Form.Group>
+              <Form.Check type="checkbox" id="all-day" name="isAllDay" onChange={this.handleToggle} checked={isAllDay} label="All Day Event?" />
+            </Form.Group>
 
-          <ButtonToolbar>
-            <Button variant="primary" size="lg" onClick={this.submitForm}>
-              Submit
-          </Button>
-          </ButtonToolbar>
+            {
+              !this.state.isAllDay &&
+              <>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="from">From</InputGroup.Text>
+                  </InputGroup.Prepend>
 
-          <Form.Group>
-            <Form.Check type="checkbox" id="all-day" name="isAllDay" onChange={this.handleToggle} checked={isAllDay} label="All Day Event?" />
-          </Form.Group>
+                  <select onChange={this.onChange} name="from">
+                    {DROPDOWN_TIMES.map((time, index) => {
+                      return <option value={60 * (index + 1)} key={index}>{time}</option>
+                    })}
+                  </select>
+                  <InputGroup.Append>
+                    <select name="fromAMPM" onChange={this.onChange}>
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
+                  </InputGroup.Append>
+                </InputGroup>
 
-          {
-            !this.state.isAllDay && 
-            <>
-            <InputGroup className="mb-3">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="from">From</InputGroup.Text>
-            </InputGroup.Prepend>
+                <InputGroup className="mb-3">
+                  <InputGroup.Prepend>
+                    <InputGroup.Text id="from">To</InputGroup.Text>
+                  </InputGroup.Prepend>
 
-            <select onChange={this.onChange} name="from">
-              {DROPDOWN_TIMES.map((time, index) => {
-                return <option value={60 * (index + 1)} key={index}>{time}</option>
-              })}
-            </select>
-            <InputGroup.Append>
-              <select name="fromAMPM" onChange={this.onChange}>
-                <option>AM</option>
-                <option>PM</option>
-              </select>
-            </InputGroup.Append>
-          </InputGroup>
+                  <select onChange={this.onChange} name="to">
+                    {DROPDOWN_TIMES.map((time, index) => {
+                      return <option value={60 * (index + 1)} key={index + 64}>{time}</option>
+                    })}
+                  </select>
+                  <InputGroup.Append>
+                    <select name="toAMPM" onChange={this.onChange}>
+                      <option>AM</option>
+                      <option>PM</option>
+                    </select>
+                  </InputGroup.Append>
+                </InputGroup>
+              </>
+            }
 
-          <InputGroup className="mb-3">
-            <InputGroup.Prepend>
-              <InputGroup.Text id="from">To</InputGroup.Text>
-            </InputGroup.Prepend>
-
-            <select onChange={this.onChange} name="to">
-              {DROPDOWN_TIMES.map((time, index) => {
-                return <option value={60 * (index + 1)} key={index + 64}>{time}</option>
-              })}
-            </select>
-            <InputGroup.Append>
-              <select name="toAMPM" onChange={this.onChange}>
-                <option>AM</option>
-                <option>PM</option>
-              </select>
-            </InputGroup.Append>
-          </InputGroup>
-          </>
-          }
-          
-          <InputGroup>
-            <select name="dayOfWeek" onChange={this.onChange}>
-              <option>Monday</option>
-              <option>Tuesday</option>
-              <option>Wednesday</option>
-              <option>Thursday</option>
-              <option>Friday</option>
-              <option>Saturday</option>
-              <option>Sunday</option>
-            </select>
-          </InputGroup>
-        </Form>
+            <Form.Label>
+              Which Day?
+            </Form.Label>
+            <InputGroup>
+              <DatePicker
+                selected={this.state.selectedDate}
+                onChange={this.handleChange}
+              />
+            </InputGroup>
+            <hr />
+            <Link to="/overview">
+              <Button variant="primary" disabled={this.state.title && this.state.description && this.state.selectedDate ? false : true} size="lg" onClick={this.submitForm}>
+                Submit
+              </Button>
+            </Link>
+          </Form>
+        </Alert>
       </div >
     );
   }
